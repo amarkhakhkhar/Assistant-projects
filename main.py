55d41b6af08e5248c467053bd7281b4618562206
Amar Khakhkhar <amarkhakhkhar2241566@gmail.com>
2021-12-07 11:27:28 +0530

    Update main.py
    Added rock paper scissors game, dice and toss simulation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import webbrowser
 import os
 import requests
 import sys
+import random
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -53,6 +54,9 @@ def optionsavailable():
     print("Speak 'open youtube' for opening youtube in your browser")
     print("Speak 'time now' for getting time right now")
     print("Speak 'You can sleep now' for closing the assistant")
+    print("Speak 'Rock paper scissors game' for playing game")
+    print("Speak 'simulate coin toss' for simulating a coin toss")
+    print("Speak 'simulate dice' for simulating a dice roll" )
 
 def sleep():
     speak("Hope you have a nice day")
@@ -97,6 +101,63 @@ def newsgetter():
 	speak = Dispatch("SAPI.Spvoice")
 	speak.Speak(results)
 
+def rpsg():
+    moves = ['rock', 'paper', 'scissors']
+    speak("Welcome to Rock Paper Scissors game. \n Select your move")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing")
+        move = r.recognize_google(audio, language='en-in')
+        userinput = f"You said: {move}"
+        speak(userinput)
+    
+    except Exception as e:
+        print("I wasn't able to understand. May you repeat Sire")
+        return "None"
+    
+
+    user_score = 0
+    comp_score = 0
+    if move == 'exit': 
+        takeCommand()
+        comp_score = 0
+        user_score = 0
+    else:  
+        user_move = move
+    cmove = random.randint(0, 2)
+    comp_move = moves[cmove]
+    speak(f"Computer chose: {comp_move}, You chose: {user_move} ")
+    if user_move == comp_move: print("Draw")
+    elif (user_move == 'rock' and comp_move == 'scissors') or (user_move == 'paper' and comp_move == 'rock') or (user_move == 'scissor' and comp_move == 'paper'):
+        print("You won, Congratulations")
+        user_score+= 1
+        print(user_score, comp_score)
+    else: 
+        print("Computer won")
+        comp_score+= 1 
+        print(f"Comp score = {comp_score},User score = {user_score}")
+        rpsg()
+
+def cointoss():
+    resulter = random.randint(0,100)
+    if resulter%2 == 0:
+        print("Result is heads")
+        speak("Result is heads")
+    else:
+        print("Result is tails")
+        speak("Result is tails")
+
+def diceroll():
+    dice_rolling = random.randint(0,5)
+    result = dice_rolling + 1
+    speak(f"Result of dice roll is {result}")
+    print(f"Result of dice roll is {result}")
+
 if __name__ == "__main__":
     wishuser()
     optionsavailable()
@@ -122,6 +183,14 @@ if __name__ == "__main__":
         elif 'time now' in query:
             timenow()
 
-        elif 'you can sleep now':
+        elif 'you can sleep now' in query:
             sleep()
+
+        elif 'rock paper scissors game' in query:
+            rpsg()
         
+        elif 'coin toss' in query:
+            cointoss()
+
+        elif 'simulate dice' in query:
+            diceroll()
